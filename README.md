aa8644b128ea3f22a50f0953432099aed7b8b938
Oliver Steele <steele@osteele.com>
2017-03-16 16:39:44 +0800

    update NSound → Sonic Pi
    For Python 3 compatibility

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # ToolBox-AlgorithmicMusic
-Algorithmic Music Composition Project Toolbox starter code
 
-Full instructions at https://sites.google.com/site/sd16spring/home/project-toolbox/algorithmic-music-composition
+Starter code for Algorithmic Music Composition Project Toolbox.
--- blues_solo.py
+++ blues_solo.py
@@ -1,36 +1,42 @@
-""" Synthesizes a blues solo algorithmically """
+"""Synthesizes a blues solo algorithmically."""
 
-from Nsound import *
-import numpy as np
+import atexit
+import os
 from random import choice
 
-def add_note(out, instr, key_num, duration, bpm, volume):
-    """ Adds a note from the given instrument to the specified stream
+from psonic import *
 
-        out: the stream to add the note to
-        instr: the instrument that should play the note
-        key_num: the piano key number (A 440Hzz is 49)
-        duration: the duration of the note in beats
-        bpm: the tempo of the music
-        volume: the volume of the note
-	"""
-    freq = (2.0**(1/12.0))**(key_num-49)*440.0
-    stream = instr.play(duration*(60.0/bpm),freq)
-    stream *= volume
-    out << stream
+# The sample directory is relative to this source directory.
+SAMPLES_DIR = os.path.join(os.path.dirname(__file__), "samples")
 
-# this controls the sample rate for the sound file you will generate
-sampling_rate = 44100.0
-Wavefile.setDefaults(sampling_rate, 16)
+SAMPLE_FILE = os.path.join(SAMPLES_DIR, "bass_D2.wav")
+SAMPLE_NOTE = D2
 
-bass = GuitarBass(sampling_rate)	# use a guitar bass as the instrument
-solo = AudioStream(sampling_rate, 1)
 
-""" these are the piano key numbers for a 3 octave blues scale in A
-	See: http://en.wikipedia.org/wiki/Blues_scale """
-blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
+def play_note(note, beats=1, bpm=60, amp=1):
+    """Plays note for `beats` beats. Returns when done."""
+    # `note` is this many half-steps higher than the sampled note
+    half_steps = note - SAMPLE_NOTE
+    # An octave higher is twice the frequency. There are twelve half-steps per octave. Ergo,
+    # each half step is a twelth root of 2 (in equal temperament).
+    rate = (2 ** (1 / 12)) ** half_steps
+    assert os.path.exists(SAMPLE_FILE)
+    # Turn sample into an absolute path, since Sonic Pi is executing from a different working directory.
+    sample(os.path.realpath(SAMPLE_FILE), rate=rate, amp=amp)
+    sleep(beats * 60 / bpm)
+
+
+def stop():
+    """Stops all tracks."""
+    msg = osc_message_builder.OscMessageBuilder(address='/stop-all-jobs')
+    msg.add_arg('SONIC_PI_PYTHON')
+    msg = msg.build()
+    synthServer.client.send(msg)
+
+atexit.register(stop)  # stop all tracks when the program exits normally or is interrupted
+
+# These are the piano key numbers for a 3-octave blues scale in A. See: http://en.wikipedia.org/wiki/Blues_scale
+blues_scale = [40, 43, 45, 46, 47, 50, 52, 55, 57, 58, 59, 62, 64, 67, 69, 70, 71, 74, 76]
 beats_per_minute = 45				# Let's make a slow blues solo
 
-add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
-
-solo >> "blues_solo.wav"
+play_note(blues_scale[0], beats=1, bpm=beats_per_minute)
