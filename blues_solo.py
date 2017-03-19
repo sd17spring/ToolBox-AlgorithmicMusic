bf3ce88e705bc07400b67f7063d0f0e2a98abe5b
Oliver Steele <steele@osteele.com>
2017-03-19 11:23:13 +0800

    psonic now has a working synthServer.stop()
    Remove the workaround

--- blues_solo.py
+++ blues_solo.py
@@ -6,11 +6,13 @@ from random import choice
 
 from psonic import *
 
-# The sample directory is relative to this source directory.
+# The sample directory is relative to this source file's directory.
 SAMPLES_DIR = os.path.join(os.path.dirname(__file__), "samples")
 
 SAMPLE_FILE = os.path.join(SAMPLES_DIR, "bass_D2.wav")
-SAMPLE_NOTE = D2
+SAMPLE_NOTE = D2  # the sample file plays at this pitch
+
+atexit.register(synthServer.stop)  # stop all tracks when the program exits normally or is interrupted
 
 
 def play_note(note, beats=1, bpm=60, amp=1):
@@ -25,15 +27,6 @@ def play_note(note, beats=1, bpm=60, amp=1):
     sample(os.path.realpath(SAMPLE_FILE), rate=rate, amp=amp)
     sleep(beats * 60 / bpm)
 
-
-def stop():
-    """Stops all tracks."""
-    msg = osc_message_builder.OscMessageBuilder(address='/stop-all-jobs')
-    msg.add_arg('SONIC_PI_PYTHON')
-    msg = msg.build()
-    synthServer.client.send(msg)
-
-atexit.register(stop)  # stop all tracks when the program exits normally or is interrupted
 
 # These are the piano key numbers for a 3-octave blues scale in A. See: http://en.wikipedia.org/wiki/Blues_scale
 blues_scale = [40, 43, 45, 46, 47, 50, 52, 55, 57, 58, 59, 62, 64, 67, 69, 70, 71, 74, 76]
